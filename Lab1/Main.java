7afa6672ff440fb2db17dd6cdabac36fca3b8450
Riserhool <125794077+Riserhool@users.noreply.github.com>
2023-09-25 17:30:55 +0300

    h

--- Lab1/Main.java
+++ Lab1/Main.java
@@ -11,7 +11,6 @@ public class Main {
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-        in.close();
         System.out.print("Ввод: ");
         String Input = in.nextLine();
         char[] InpChar = new char[10];
